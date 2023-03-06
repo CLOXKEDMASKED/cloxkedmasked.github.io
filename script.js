2de332efd30246644ab0c09a299d02fb13e1ed32
MXSKED <100958735+CLOXKEDMASKED@users.noreply.github.com>
2023-03-06 10:58:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ let calcScrollValue = () => {
 
 window.onscroll = calcScrollValue;
 window.onload = calcScrollValue;
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
