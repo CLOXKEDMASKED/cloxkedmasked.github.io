7cbd6fd43ea6497db3704f7ee2d698b453e47361
MXSKED <100958735+CLOXKEDMASKED@users.noreply.github.com>
2023-03-07 07:32:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ let calcScrollValue = () => {
 };
 
 window.onscroll = calcScrollValue;
+let container = document.querySelector(".container");
+let timeNow = new Date().getHours();
+let greeting =
+  timeNow >= 5 && timeNow < 12
+    ? "Good Morning"
+    : timeNow >= 12 && timeNow < 18
+    ? "Good Afternoon"
+    : "Good evening";
+container.innerHTML = `<h1>${greeting}</h1>`;
